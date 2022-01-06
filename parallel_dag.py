ec5fc8124cb71b1435fc97442ad9df873bcca485
mahesh-shakwar <mshakwar@cazena.com>
2022-01-06 17:12:10 +0530

    just added a comment

--- parallel_dag.py
+++ parallel_dag.py
@@ -4,6 +4,7 @@ from airflow.operators.python_operator import PythonOperator
 
 from datetime import datetime
 
+# editing, just adding a comment.
 default_args = {
     'start_date': datetime(2019, 1, 1),
     'owner': 'Airflow',
